60073116fc5374e30981f54f1fa101e979d81efb
Javid Ahmed <javidahmed@icloud.com>
2024-04-14 00:38:54 +0100

    Update main

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from src.output_1.blinking_led_1 import BlinkingLED
 from src.output_2.rgb_led import RGBLED
+from src.output_3.led_bar import LEDBar
 
 if __name__ == "__main__":
-    app = RGBLED.app(red_pin=17, green_pin=18, blue_pin=27)
+    app = LEDBar.app(led_pins=[11, 12, 13, 15, 16, 18, 22, 3, 5, 24])
     app.run()
