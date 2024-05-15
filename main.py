9cf899cee1fe9367b92e1fd255676e114a0fdcb8
Javid Ahmed <javidahmed@icloud.com>
2024-05-15 23:09:19 +0100

    Add Button app

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from src.input_1_1.button import Button
 from src.output_1.blinking_led import BlinkingLED
 from src.output_2.rgb_led import RGBLED
 from src.output_3.led_bar import LEDBar
 
 if __name__ == "__main__":
-    app = LEDBar.app(led_pins=[11, 12, 13, 15, 16, 18, 22, 3, 5, 24])
+    app = Button.app(led_pin=17, button_pin=18)
     app.run()
