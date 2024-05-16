d0c0c99e4c009a20247d614df280e08a99629813
Javid Ahmed <javidahmed@icloud.com>
2024-05-16 21:02:04 +0100

    Update

--- src/input_2_2_3/2.2.3_DHT.py
+++ src/input_2_2_3/2.2.3_DHT.py
@@ -1,4 +1,4 @@
-'''
+"""
 **********************************************************************
 * Filename    : dht11.py
 * Description : test for SunFoudner DHT11 humiture & temperature module
@@ -7,12 +7,15 @@
 * Website     : www.sunfounder.com
 * Update      : Dream    2016-09-30    New release
 **********************************************************************
-'''
-#!/usr/bin/env python3
-import RPi.GPIO as GPIO
+"""
+
 import time
 
+import RPi.GPIO as GPIO
+
 dhtPin = 17
+tempPins = [6, 13, 19]
+humPins = [16, 20, 21]
 
 GPIO.setmode(GPIO.BCM)
 
@@ -24,111 +27,117 @@ STATE_DATA_FIRST_PULL_DOWN = 3
 STATE_DATA_PULL_UP = 4
 STATE_DATA_PULL_DOWN = 5
 
+
 def readDht11():
-	GPIO.setup(dhtPin, GPIO.OUT)
-	GPIO.output(dhtPin, GPIO.HIGH)
-	time.sleep(0.05)
-	GPIO.output(dhtPin, GPIO.LOW)
-	time.sleep(0.02)
-	GPIO.setup(dhtPin, GPIO.IN, GPIO.PUD_UP)
+    GPIO.setup(dhtPin, GPIO.OUT)
+    GPIO.output(dhtPin, GPIO.HIGH)
+    time.sleep(0.05)
+    GPIO.output(dhtPin, GPIO.LOW)
+    time.sleep(0.02)
+    GPIO.setup(dhtPin, GPIO.IN, GPIO.PUD_UP)
 
-	unchanged_count = 0
-	last = -1
-	data = []
-	while True:
-		current = GPIO.input(dhtPin)
-		data.append(current)
-		if last != current:
-			unchanged_count = 0
-			last = current
-		else:
-			unchanged_count += 1
-			if unchanged_count > MAX_UNCHANGE_COUNT:
-				break
+    unchanged_count = 0
+    last = -1
+    data = []
+    while True:
+        current = GPIO.input(dhtPin)
+        data.append(current)
+        if last != current:
+            unchanged_count = 0
+            last = current
+        else:
+            unchanged_count += 1
+            if unchanged_count > MAX_UNCHANGE_COUNT:
+                break
 
-	state = STATE_INIT_PULL_DOWN
+    state = STATE_INIT_PULL_DOWN
 
-	lengths = []
-	current_length = 0
+    lengths = []
+    current_length = 0
 
-	for current in data:
-		current_length += 1
+    for current in data:
+        current_length += 1
 
-		if state == STATE_INIT_PULL_DOWN:
-			if current == GPIO.LOW:
-				state = STATE_INIT_PULL_UP
-			else:
-				continue
-		if state == STATE_INIT_PULL_UP:
-			if current == GPIO.HIGH:
-				state = STATE_DATA_FIRST_PULL_DOWN
-			else:
-				continue
-		if state == STATE_DATA_FIRST_PULL_DOWN:
-			if current == GPIO.LOW:
-				state = STATE_DATA_PULL_UP
-			else:
-				continue
-		if state == STATE_DATA_PULL_UP:
-			if current == GPIO.HIGH:
-				current_length = 0
-				state = STATE_DATA_PULL_DOWN
-			else:
-				continue
-		if state == STATE_DATA_PULL_DOWN:
-			if current == GPIO.LOW:
-				lengths.append(current_length)
-				state = STATE_DATA_PULL_UP
-			else:
-				continue
-	if len(lengths) != 40:
-		#print ("Data not good, skip")
-		return False
+        if state == STATE_INIT_PULL_DOWN:
+            if current == GPIO.LOW:
+                state = STATE_INIT_PULL_UP
+            else:
+                continue
+        if state == STATE_INIT_PULL_UP:
+            if current == GPIO.HIGH:
+                state = STATE_DATA_FIRST_PULL_DOWN
+            else:
+                continue
+        if state == STATE_DATA_FIRST_PULL_DOWN:
+            if current == GPIO.LOW:
+                state = STATE_DATA_PULL_UP
+            else:
+                continue
+        if state == STATE_DATA_PULL_UP:
+            if current == GPIO.HIGH:
+                current_length = 0
+                state = STATE_DATA_PULL_DOWN
+            else:
+                continue
+        if state == STATE_DATA_PULL_DOWN:
+            if current == GPIO.LOW:
+                lengths.append(current_length)
+                state = STATE_DATA_PULL_UP
+            else:
+                continue
+    if len(lengths) != 40:
+        # print ("Data not good, skip")
+        return False
 
-	shortest_pull_up = min(lengths)
-	longest_pull_up = max(lengths)
-	halfway = (longest_pull_up + shortest_pull_up) / 2
-	bits = []
-	the_bytes = []
-	byte = 0
+    shortest_pull_up = min(lengths)
+    longest_pull_up = max(lengths)
+    halfway = (longest_pull_up + shortest_pull_up) / 2
+    bits = []
+    the_bytes = []
+    byte = 0
 
-	for length in lengths:
-		bit = 0
-		if length > halfway:
-			bit = 1
-		bits.append(bit)
-	#print ("bits: %s, length: %d" % (bits, len(bits)))
-	for i in range(0, len(bits)):
-		byte = byte << 1
-		if (bits[i]):
-			byte = byte | 1
-		else:
-			byte = byte | 0
-		if ((i + 1) % 8 == 0):
-			the_bytes.append(byte)
-			byte = 0
-	#print (the_bytes)
-	checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
-	if the_bytes[4] != checksum:
-		#print ("Data not good, skip")
-		return False
+    for length in lengths:
+        bit = 0
+        if length > halfway:
+            bit = 1
+        bits.append(bit)
+    # print ("bits: %s, length: %d" % (bits, len(bits)))
+    for i in range(0, len(bits)):
+        byte = byte << 1
+        if bits[i]:
+            byte = byte | 1
+        else:
+            byte = byte | 0
+        if (i + 1) % 8 == 0:
+            the_bytes.append(byte)
+            byte = 0
+    # print (the_bytes)
+    checksum = (the_bytes[0] + the_bytes[1] + the_bytes[2] + the_bytes[3]) & 0xFF
+    if the_bytes[4] != checksum:
+        # print ("Data not good, skip")
+        return False
 
-	return the_bytes[0], the_bytes[2]
+    return the_bytes[0], the_bytes[2]
+
 
 def main():
+    for pin in tempPins + humPins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+    while True:
+        result = readDht11()
+        if result:
+            humidity, temperature = result
+            print("humidity: %s %%,  Temperature: %s C`" % (humidity, temperature))
+        time.sleep(1)
 
-	while True:
-		result = readDht11()
-		if result:
-			humidity, temperature = result
-			print ("humidity: %s %%,  Temperature: %s C`" % (humidity, temperature))
-		time.sleep(1)
 
 def destroy():
-	GPIO.cleanup()
+    GPIO.cleanup()
 
-if __name__ == '__main__':
-	try:
-		main()
-	except KeyboardInterrupt:
-		destroy() 
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        destroy()
